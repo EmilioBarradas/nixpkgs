ad39d95870c0392df527400401321a8fa5e79ba7
Emilio Barradas <decalerss@gmail.com>
2024-09-15 22:10:51 -0400

    maintainers: add emilio-barradas

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6161,6 +6161,11 @@
     githubId = 428026;
     name = "embr";
   };
+  emilio-barradas = {
+    github = "EmilioBarradas";
+    githubId = 16023547;
+    name = "Emilio Barradas";
+  };
   emilioziniades = {
     email = "emilioziniades@protonmail.com";
     github = "emilioziniades";
